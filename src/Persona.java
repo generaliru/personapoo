c498804cb11d0b0008ee802d1b840183919db339
generaliru <ucamcahoa1600@alumno.ipn.mx>
2021-07-15 18:04:54 -0500

    Ejemplo Persona para clases

---
+++ src/Persona.java
@@ -0,0 +1,14 @@
+
+public class Persona {
+	String name;
+	String Email;
+	int age;
+	
+	public String saludar() {
+		return "Hola soy " + this.name;
+	}
+	
+	public String miEdad() {
+		return "Mi edad es " + this.age;
+	}
+}
---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona uriel = new Persona();
+		uriel.name = "Uriel German";
+		uriel.age = 23;
+		System.out.println(uriel.saludar());
+		System.out.println(uriel.miEdad());
+	}
+
+}
